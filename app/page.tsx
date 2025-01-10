f80f82b289960d5616f2292151303eff8585ccdd
sen442b <shubhendusen322@gmail.com>
2025-01-11 00:08:29 +0530

    feat: cal embed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
+"use client";
+
 import Image from "next/image";
 import { socialLinks } from "./config";
+import { getCalApi } from "@calcom/embed-react";
+import { useEffect } from "react";
 
 export default function Page() {
+  useEffect(() => {
+    (async function () {
+      const cal = await getCalApi({ namespace: "15min" });
+      cal("floatingButton", {
+        calLink: "shubhendu-sen/15min",
+        config: { layout: "month_view", theme: "dark" },
+        buttonPosition: "bottom-right",
+        buttonText: "Let's Talk",
+      });
+      cal("ui", {
+        hideEventTypeDetails: false,
+        layout: "month_view",
+      });
+    })();
+  }, []);
   return (
     <section>
       <a href={socialLinks.bluesky} target="_blank">
@@ -17,35 +36,45 @@ export default function Page() {
       </a>
 
       <h1 className="mb-8 text-2xl font-medium tracking-tight">
-      Developer, Thinker, Builder.
+        Developer, Thinker, Builder.
       </h1>
 
       <div className="prose prose-neutral dark:prose-invert">
         <p>
-        I'm a freelance developer with 2 years of hands-on experience in the JavaScript ecosystem. I enjoy building everything from smooth, interactive frontends to solid, reliable backends. Turning ideas into real, usable solutions? That's my thing.
+          I'm a freelance developer with 2 years of hands-on experience in the
+          JavaScript ecosystem. I enjoy building everything from smooth,
+          interactive frontends to solid, reliable backends. Turning ideas into
+          real, usable solutions? That's my thing.
         </p>
-     
+
         <div>
-        <h2 className="text-3xl font-bold mb-4">What I Do</h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-  <div>
-    <h3 className="text-lg font-semibold">Frontend Development</h3>
-    <p className="text-gray-600">React, Next.js, Vite, Zustand, Material UI  — crafting engaging UIs.</p>
-  </div>
+          <h2 className="text-3xl font-bold mb-4">What I Do</h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            <div>
+              <h3 className="text-lg font-semibold">Frontend Development</h3>
+              <p className="text-gray-600">
+                React, Next.js, Vite, Zustand, Material UI — crafting engaging
+                UIs.
+              </p>
+            </div>
 
-  <div>
-    <h3 className="text-lg font-semibold">Backend Development</h3>
-    <p className="text-gray-600">Node.js, Express, Prisma — building robust APIs and managing databases with PostgreSQL and (or) MongoDB.</p>
-  </div>
+            <div>
+              <h3 className="text-lg font-semibold">Backend Development</h3>
+              <p className="text-gray-600">
+                Node.js, Express, Prisma — building robust APIs and managing
+                databases with PostgreSQL and (or) MongoDB.
+              </p>
+            </div>
 
-  <div>
-    <h3 className="text-lg font-semibold">Hosting</h3>
-    <p className="text-gray-600">DigitalOcean, AWS S3 — deploying scalable and reliable applications and hosting assets.</p>
-  </div>
-</div>
-</div>
-
-
+            <div>
+              <h3 className="text-lg font-semibold">Hosting</h3>
+              <p className="text-gray-600">
+                DigitalOcean, AWS S3 — deploying scalable and reliable
+                applications and hosting assets.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
